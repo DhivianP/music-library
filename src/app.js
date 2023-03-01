7911b748722a3a6a793738f1f96c809c9b59ac33
Dhivian Pirabaharan <dhivian1234@hotmail.co.uk>
2023-03-01 21:07:08 +0000

    create artist first test

--- src/app.js
+++ src/app.js
@@ -1,12 +1,11 @@
 const express = require('express');
+const artistRouter = require('./routes/artist')
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.status(200).send('Hello World');
-});
+app.use('/artists', artistRouter);
 
 
 module.exports = app;
