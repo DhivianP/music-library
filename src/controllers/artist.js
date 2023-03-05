67b06b80046397b856aa86b46d1bc075f5a57271
Dhivian Pirabaharan <dhivian1234@hotmail.co.uk>
2023-03-05 13:00:10 +0000

    Reading Artists/Reading single Artists

--- src/controllers/artist.js
+++ src/controllers/artist.js
@@ -4,11 +4,37 @@ const createArtist = async (req, res) => {
     const { name, genre } = req.body
   
     try {
-      const { rows: [ artist ] } = await db.query(`INSERT INTO Artists (name, genre) VALUES ('${name}', '${genre}') RETURNING *`)
+      const { rows: [ artist ] } = await db.query('INSERT INTO Artists (name, genre) VALUES ($1, $2) RETURNING *', [name, genre])
       res.status(201).json(artist)
     } catch (err) {
       res.status(500).json(err.message)
     }
   }
 
-module.exports = {createArtist}
+  const getAllArtists = async (_, res) => {
+    try {
+      const { rows } = await db.query('SELECT * FROM Artists')
+      res.status(200).json(rows)
+    } catch (err) {
+      res.status(500).json(err.message)
+    }
+  }
+
+  const getArtistById = async (req, res) => {
+    try {
+      const { id } = req.params
+      const { rows: [ artist ] } = await db.query('SELECT * FROM Artists WHERE id = $1', [ id ])
+  
+      if (!artist) {
+        return res.status(404).json({ message: `artist ${id} does not exist` })
+      }
+  
+      res.status(200).json(artist)
+    } catch (err) {
+      res.status(500).json(err.message)
+    }
+  }
+
+  
+
+module.exports = {createArtist, getAllArtists,getArtistById}
--- src/routes/artist.js
+++ src/routes/artist.js
@@ -5,4 +5,8 @@ const router = express.Router();
 
 router.post('/', artistController.createArtist);
 
+router.get('/', artistController.getAllArtists);
+
+router.get('/:id', artistController.getArtistById)
+
 module.exports = router;
